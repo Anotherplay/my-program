e593ae5a3f0b37cfd43f7c5128c6ff101e5b299b
Anotherplay <novianrifqitri@gmail.com>
2026-01-19 03:05:28 +0000

    buat bikin angka ganjil genap

---
+++ ganjilgenap.py
@@ -0,0 +1,8 @@
+bil = int(input('masukkan bilangan: '))
+
+if bil % 2 == 0:
+    print(f'{bil} adalah angka genap')
+else:
+    print(f'{bil} adalah angka ganjil')
+
+print('kode ini saya buat melalui codespaces')
